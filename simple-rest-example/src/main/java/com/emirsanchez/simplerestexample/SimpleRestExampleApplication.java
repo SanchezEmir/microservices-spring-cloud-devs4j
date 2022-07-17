b3e517a0ac530831869c5ec35e69478ecf3758c3
SanchezEmir <emir2104@gmail.com>
2022-07-17 16:37:47 -0500

    add project example

---
+++ simple-rest-example/src/main/java/com/emirsanchez/simplerestexample/SimpleRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.emirsanchez.simplerestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestExampleApplication.class, args);
+    }
+
+}
